6964a93aaed96a5a01bed178010e0f0be3ffd696
Matthijs4004 <matthijs4004@gmail.com>
2022-01-06 20:56:07 +0100

    finished grabbelton.py

--- grabbelton.py
+++ grabbelton.py
@@ -1,4 +1,6 @@
-import random,time
+import tkinter as tk
+from tkinter.constants import BOTTOM
+import random
 # Grabbelton
 
 # Een GUI opend met een knop er in waarop grabbelen staat
@@ -8,26 +10,42 @@ import random,time
 # Daarna moet het aantal items worden aangepast en mag dat woord niet meer terug komen.
 # gum, sleutelhanger, stuiterbal, potloden, spelletje, puzzel, snoep, zaklamp, stickers, stempels
 
-grabbelton = ["gum","sleutelhanger","stuiterbal","potlood","spelletje","puzzel","snoep","zaklamp","stickers","stempels"]
+grabbelton = ["gum","sleutelhanger","stuiterbal","potlood","spelletje","puzzel","snoepje","zaklamp","sticker","stempel"]
 
-
-def grabbelen(grabbelton):
+def grabbelen():
     item = random.choice(grabbelton)
-    print("Gefeliciteerd, je hebt een", item ,"gegrabbeld")
+    Text.set("Gefeliciteerd, je hebt een "+ item +" gegrabbeld")
+    label2.configure(textvariable=Text)
     grabbelton.remove(item)
     
     if len(grabbelton) > 0:
-        print("Er zitten nog", len(grabbelton), "items in de grabbelton")
+        Text2.set("Er zitten nog "+ str(len(grabbelton)) +" items in de grabbelton")
+        label3.configure(textvariable=Text2)
+    elif len(grabbelton) == 0:
+        Text.set("De grabbelton is leeg")
+        label2.configure(textvariable=Text)
+        Text2.set("")
+        label3.configure(textvariable=Text2)
 
-while len(grabbelton) > 0:
-    time.sleep(0.5)
-    input("Druk op ENTER om een item uit de grabbelton te pakken ")
-    print()
-    grabbelen(grabbelton)
-        
-    if len(grabbelton) == 0:
-        print("De grabbelton is leeg")
+window = tk.Tk()
+window.geometry("400x100")
+window.title("Grabbelton")
+window["bg"] = "white"
+window.eval('tk::PlaceWindow . center') # optioneel (puur om het scherm in het midden te krijgen)
+Text = tk.StringVar(value="")
+Text2 = tk.StringVar(value="")
+label = tk.Label(text="Druk op ENTER om een item uit de grabbelton te pakken", bg="white")
+label.pack()
+label2 = tk.Label(textvariable=Text, bg="white")
+label2.pack()
+label3 = tk.Label(textvariable=Text2, bg="white")
+label3.pack()
+button = tk.Button(text="Grabbelen!",bd=0, command=grabbelen)
+button.pack(side=BOTTOM)
+grabbelton = ["gum","sleutelhanger","stuiterbal","potlood","spelletje","puzzel","snoep","zaklamp","stickers","stempels"]
 
+
+window.mainloop()
 
 
 
